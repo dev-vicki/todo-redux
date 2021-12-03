9da0ef05570d232d71c8f7bef07ae9ad1576f5e3
dev-vicki <vignesh.r1999@gmail.com>
2021-12-03 16:40:02 +0530

    used useSelector to see changes in the todo state.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { useSelector } from "react-redux";
 
 function App() {
+  const state = useSelector((state) => ({...state}));
+  console.log("state", state);
   return (
     <div className="App">
-
+        <h1>State</h1>
     </div>
   );
 }
